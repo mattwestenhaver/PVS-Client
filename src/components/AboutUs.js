5dda917b2af7f1e6f1b80801b58014c9675c4fb2
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2018-07-12 23:56:19 -0700

    about page header

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,7 +4,9 @@ class AboutUs extends React.Component {
   render() {
     return (
       <div className='about-container'>
-        <h1>Our Story</h1>
+        <div className='about-header'>
+          <h1>Our Story</h1>
+        </div>
         <div className='about-section-1'>
           <p>At Premiere Valet Parking Service we provide the Highest Standard of Private Valet Parking and Valet Parking Management Services. If you are looking for valet services in Los Angeles, Hollywood, Beverly Hills, Orange County, or Ventura County we are ready to meet your valet parking attendant needs.</p>
           <p>For over 25 years Premiere Valet Service, Inc., has served all of Southern California with valet parking service. With offices in Los Angeles and Laguna Beach we provide valet service with locally based valet parking attendants for all of Los Angeles County, Orange County, Ventura County  and Riverside County.</p>

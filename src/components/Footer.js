0e63a7a904d7c6fa21063617082337539c62f372
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2018-07-19 19:21:26 -0700

    added fb + instagram icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,8 @@ class Footer extends React.Component {
         <h5>The Highest Quality Valet Parking Service In Southern California</h5>
         <a target='_blank' href='https://www.yelp.com/biz/premiere-valet-service-los-angeles-3'><Icon name='yelp' size='big' /></a>
         <a target='_blank' href='https://www.linkedin.com/company/premiere-valet-services-inc./'><Icon name='linkedin' size='big' /></a>
+        <a target='_blank' href='https://www.facebook.com/premierevaletservices/'><Icon name='facebook' size='big' /></a>
+        <a target='_blank' href='https://www.instagram.com/premierevalet/'><Icon name='instagram' size='big' /></a>
         <h5>info@premierevaletparking.com</h5>
         <h5>(323) 876-5816</h5>
         {/* <h5>Orange County: (949) 497-8811</h5>

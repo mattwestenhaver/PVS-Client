e78a8d79d8e112374190e248d453883c59700cf6
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2018-09-13 10:18:37 -0400

    city page changes

--- src/components/City.js
+++ src/components/City.js
@@ -38,8 +38,13 @@ class City extends React.Component {
         <div className='city-section-2'>
           <hr/><h1>{this.props.city} Valet Services</h1><hr/>
           <p>There are many {this.props.city} Valet Companies, but Premiere Valet Services, Inc. is proudly the number one in {this.props.city}. If you are hosting an event in {this.props.city} and need valet, Premiere Valet Services, Inc. can offer you first class valet services and a level of service that no other valet company in the area can.</p>
-          <p>With over 25 years in the valet business, Premiere Valet Services, Inc. is the number one valet service in all of Southern California. We are the first choice valet company for all private events; from pre-Oscar private parties to small home gatherings. Parking in {this.props.city} can be difficult, but with Premiere Valet Services, Inc. you and your guests can enjoy your next private event with peace of mind.</p>
-          <p>Our <span className='italic'>premiere</span> valet parking staff is the most qualified, experienced and reliable valet parking staff you will find in any {this.props.city} valet company. Contact us any time at <a href='tel:+13238765816'>(323) 876-5816</a> or click <Link to='/quote'>here</Link> and let us make the best of your next valet parking experience.</p>
+          <p>With over 30 years in the valet business, Premiere Valet Services, Inc. is the number one valet service in all of Southern California. We are the first choice valet company for all private events; from pre-Oscar private parties to small home gatherings. Parking in {this.props.city} can be difficult, but with Premiere Valet Services, Inc. you and your guests can enjoy your next private event with peace of mind.</p>
+          <p>Our <span className='italic'>premiere</span> valet parking staff is the most qualified, experienced and reliable valet parking staff you will find in any {this.props.city} valet company. Contact us any time at 
+          {this.props.city === "Irvine" || this.props.city === "Laguna Beach" || this.props.city === "Newport Coast" || this.props.city === "Newport Beach" || this.props.city === "Emerald Bay" || this.props.city === "Corona del Mar" || this.props.city === "Coto de Caza" || this.props.city === "Pelican Hill" || this.props.city === "Dana Point" 
+            ? <a href='tel:+19494978811'> (949) 497-8811 </a> 
+            : <a href='tel:+13238765816'> (323) 876-5816 </a>
+          } 
+          or click <Link to='/quote'>here</Link> and let us make the best of your next valet parking experience.</p>
         </div>
       </div>
     )

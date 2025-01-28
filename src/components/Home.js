37bbf1e79607a9109e01a3dd46f9a7cb02d95c74
mattwestenhaver <matthew.westenhaver@gmail.com>
2025-01-27 20:46:16 -0500

    Cleanup home and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
 import { Helmet } from 'react-helmet';
@@ -19,8 +19,8 @@ export const logPageView = () => {
     ReactGA.pageview(window.location.pathname);
 };
 
-class Home extends React.Component {
-    componentDidMount() {
+const Home = () => {
+    useEffect(() => {
         window.scrollTo(0, 0);
         initGA();
         logPageView();
@@ -35,98 +35,94 @@ class Home extends React.Component {
         setTimeout(() => {
             $('.home-section-1').stop().addClass('home-picture-fade');
         }, 3400);
-    }
+    }, []);
 
-    render() {
-        return (
-            <div>
-                <Helmet>
-                    <meta charSet="utf-8" />
-                    <meta
-                        name="keywords"
-                        content="valet parking, Los Angeles valet services, valet, valet companies, los angeles valet company, los angeles valet companies, valet, valet companies los angeles, valet parking service, valet parking"
-                    />
-                    <title>Premiere Valet Services, Inc.</title>
-                </Helmet>
-                <div className="home-section-1">
-                    <img src={logo} alt="PVS Logo" />
-                    <br />
-                    <Link to="/quote">
-                        <Button color="grey" size="massive">
-                            Request A Quote
-                        </Button>
-                    </Link>
-                </div>
-                <div className="home-section-2">
-                    <p>
-                        Premiere Valet Services, Inc., provides the highest standard of private valet parking and valet
-                        parking management services. We provide valet parking service to private events with as few as
-                        25-30 guests to movie studio and industry events for 1,000’s of guests. If you are looking for a
-                        Los Angeles valet parking services, Hollywood valet parking services, Beverly Hills valet
-                        parking services, Orange County valet parking services or Ventura County valet parking services
-                        we are ready to meet your valet parking service needs.
-                    </p>
-                    <p>
-                        For over 30 years Premiere Valet Services, Inc., has served all of Southern California with the
-                        highest quality of valet parking service. With offices in Los Angeles and Laguna Beach we
-                        provide valet parking service with locally based valet parking attendants throughout Los Angeles
-                        County, Orange County and Ventura County. Our valet parking service consists of both male and
-                        female valet parking attendants.
-                    </p>
-                    <p>
-                        Not all valet parking companies are the same and we take pride in the level of valet parking
-                        service we provide. Our commitment to quality is why we are considered the Premiere valet
-                        company throughout Los Angeles, Beverly Hills, Malibu, Thousand Oaks, Newport Coast, Laguna
-                        Beach, Orange County, etc.
-                    </p>
-                    <p>
-                        Whether you are searching for a Beverly Hills valet parking company to provide Beverly Hills
-                        valet parking service or a Thousand Oaks valet parking company to provide Thousand Oaks Valet
-                        Parking Service, Premiere Valet Services, Inc., is ready to assist with all of your valet
-                        parking needs.
-                    </p>
-                    <hr />
-                </div>
-                <div className="home-section-3">
-                    <div>
-                        <div className="header-2">
-                            <h3>We Proudly Provide Valet Parking For:</h3>
-                        </div>
-                        <h4>Private Events:</h4>
-                        <ul>
-                            <li>Wedding Valet Parking</li>
-                            <li>Private Party Valet Parking</li>
-                            <li>Private Dinner Valet Parking</li>
-                            <li>Fundraiser Valet Parking</li>
-                            <li>Other Special Events</li>
-                        </ul>
-                        <h4>Corporate Events:</h4>
-                        <ul>
-                            <li>Banquet Valet Parking</li>
-                            <li>Gala Valet Parking</li>
-                            <li>Seminar Valet Parking</li>
-                        </ul>
-                        <h4>Businesses:</h4>
-                        <ul>
-                            <li>Parking Lot Attendants</li>
-                            <li>Hotel Parking Management</li>
-                            <li>Concert & Festival Parking Management</li>
-                        </ul>
+    return (
+        <div className="home-container">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <meta
+                    name="keywords"
+                    content="valet parking, Los Angeles valet services, valet, valet companies, los angeles valet company, los angeles valet companies, valet, valet companies los angeles, valet parking service, valet parking"
+                />
+                <title>Premiere Valet Services, Inc.</title>
+            </Helmet>
+            <div className="home-section-1">
+                <img src={logo} alt="PVS Logo" />
+                <br />
+                <Link to="/quote">
+                    <Button color="grey" size="massive">
+                        Request A Quote
+                    </Button>
+                </Link>
+            </div>
+            <div className="home-section-2">
+                <p>
+                    Premiere Valet Services, Inc., provides the highest standard of private valet parking and valet
+                    parking management services. We provide valet parking service to private events with as few as 25-30
+                    guests to movie studio and industry events for 1,000’s of guests. If you are looking for a Los
+                    Angeles valet parking services, Hollywood valet parking services, Beverly Hills valet parking
+                    services, Orange County valet parking services or Ventura County valet parking services we are ready
+                    to meet your valet parking service needs.
+                </p>
+                <p>
+                    For over 30 years Premiere Valet Services, Inc., has served all of Southern California with the
+                    highest quality of valet parking service. With offices in Los Angeles and Laguna Beach we provide
+                    valet parking service with locally based valet parking attendants throughout Los Angeles County,
+                    Orange County and Ventura County. Our valet parking service consists of both male and female valet
+                    parking attendants.
+                </p>
+                <p>
+                    Not all valet parking companies are the same and we take pride in the level of valet parking service
+                    we provide. Our commitment to quality is why we are considered the Premiere valet company throughout
+                    Los Angeles, Beverly Hills, Malibu, Thousand Oaks, Newport Coast, Laguna Beach, Orange County, etc.
+                </p>
+                <p>
+                    Whether you are searching for a Beverly Hills valet parking company to provide Beverly Hills valet
+                    parking service or a Thousand Oaks valet parking company to provide Thousand Oaks Valet Parking
+                    Service, Premiere Valet Services, Inc., is ready to assist with all of your valet parking needs.
+                </p>
+                <hr />
+            </div>
+            <div className="home-section-3">
+                <div>
+                    <div className="header-2">
+                        <h3>We Proudly Provide Valet Parking For:</h3>
                     </div>
-                    <div>
-                        <img src={section3Image} alt="" />
-                    </div>
+                    <h4>Private Events:</h4>
+                    <ul>
+                        <li>Wedding Valet Parking</li>
+                        <li>Private Party Valet Parking</li>
+                        <li>Private Dinner Valet Parking</li>
+                        <li>Fundraiser Valet Parking</li>
+                        <li>Other Special Events</li>
+                    </ul>
+                    <h4>Corporate Events:</h4>
+                    <ul>
+                        <li>Banquet Valet Parking</li>
+                        <li>Gala Valet Parking</li>
+                        <li>Seminar Valet Parking</li>
+                    </ul>
+                    <h4>Businesses:</h4>
+                    <ul>
+                        <li>Parking Lot Attendants</li>
+                        <li>Hotel Parking Management</li>
+                        <li>Concert & Festival Parking Management</li>
+                    </ul>
                 </div>
-                <div className="home-section-4">
-                    <Link to="/quote">
-                        <Button className="request-home-2" color="grey" size="massive">
-                            Request A Quote
-                        </Button>
-                    </Link>
+                <div>
+                    <img src={section3Image} alt="" />
                 </div>
             </div>
-        );
-    }
-}
+            <div className="home-section-4">
+                <Link to="/quote">
+                    <Button className="request-home-2" color="grey" size="massive">
+                        Request A Quote
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
+};
 
 export default Home;
